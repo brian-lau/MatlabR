e15458a520c428560d0770c96a8aec3d15575afb
Brian Lau <brian.lau@upmc.fr>
2016-06-20 15:51:30 +0200

    trying to plot

--- MatR.m
+++ MatR.m
@@ -5,7 +5,7 @@ classdef MatR < handle
       result
       pid
    end
-   
+
    properties (Dependent = true)
       isConnected
    end
@@ -13,13 +13,21 @@ classdef MatR < handle
    methods
       function self = MatR()
          try
-            import org.rosuda.REngine.*;
-            import org.rosuda.REngine.Rserve.*;
+            self.connect();
          catch
-            
+            % TODO 
+            path = fileparts(which('MatR'));
+            javaaddpathstatic([path filesep 'lib/REngine.jar'])
+            javaaddpathstatic([path filesep 'lib/RserveEngine.jar'])
+            % search or download jars
+            % add to static path
+            % https://rforge.net/Rserve/files/REngine.jar
+            % https://rforge.net/Rserve/files/RserveEngine.jar
+            %clear java;
+            self.connect();
          end
          
-         self.connect();
+         %self.connect();
          path = fileparts(which('MatR'));
          self.source(['"' path filesep 'utils.R' '"']);
       end
@@ -33,7 +41,7 @@ classdef MatR < handle
       end
       
       function connect(self,host,port)
-         import org.rosuda.REngine.Rserve.RConnection;
+         import org.rosuda.REngine.Rserve.*;
          self.connection = RConnection();
          
          fprintf(1,'%s\n',self.Rversion);
@@ -71,6 +79,14 @@ classdef MatR < handle
          list = cell(list.asStrings);
       end
       
+      function wd = pwd(self)
+         wd = char(self.connection.eval('getwd()').asString());
+      end
+      
+      function list = ls(self)
+         list = cell(self.connection.eval('list.files()').asStrings());
+      end
+      
       function source(self,path)
          assert(ischar(path),'Input must be a string');
          self.eval(['source(' path ')']);
@@ -85,13 +101,50 @@ classdef MatR < handle
          self.result = self.connection.eval(expression);
       end
       
+      function voidEval(self,expression)
+         self.connection.voidEval(expression);
+      end
+      
+      function frame = plot(self,expression)
+%          import javax.swing.*
+%          
+%          frame = JFrame('test');
+%          frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+%          %frame.removeAll();
+         id = char(java.util.UUID.randomUUID.toString);
+         self.eval(['jpeg("' [id '.jpg'] '",width=1000,height=1000,quality=100)']);
+         self.voidEval(expression);
+         self.voidEval('dev.off()');
+%          
+%          image = JLabel();
+%          image.setIcon([]);
+%          image.setIcon(ImageIcon([self.pwd filesep [id '.jpg']]));
+%          %JLabel()
+%          %frame.setIconImage(image);
+%          %frame.add(image);
+%          %frame.validate();
+%          %frame.add(JLabel(ImageIcon('/Users/brian/Documents/Code/Repos/MatlabR/01.jpeg')));
+%          frame.setSize(450,450);
+%          frame.setVisible(1);
+         figure;
+         img = imread([self.pwd filesep [id '.jpg']]);
+         image(img);
+         axis off;
+         axis image;
+      end
+      
+      function assign(self,name,value)
+         self.connection.assign(MatR.jstr(name),value);
+      end
+      
       function delete(self)
          self.kill();
          self.close();
+         % TODO remove temp directories
       end
       
       function self = kill(self)
-         import org.rosuda.REngine.Rserve.RConnection;
+         import org.rosuda.REngine.Rserve.*;
          temp = RConnection();
          % SIGTERM might not be understood everywhere: so using SIGKILL signal, as well. 
          temp.eval(['tools::pskill(' num2str(self.pid) ')']); 
@@ -102,7 +155,21 @@ classdef MatR < handle
    
    methods(Static)
       
-      % Construct Java represenation of data.frame
+      function str = jstr(x)
+         if iscell(x)
+            n = numel(x);
+            str = javaArray('java.lang.String',n);
+            for j = 1:n
+               str(j) = java.lang.String(x{j});
+            end
+         elseif ischar(x)
+            str = java.lang.String(x);
+         else
+            error('Bad input');
+         end
+      end
+      
+      % Construct Java represention of data.frame
       function df = dataframe(x)
          import org.rosuda.REngine.*;
          l = RList;
@@ -111,16 +178,12 @@ classdef MatR < handle
 
          switch class(x)
             case 'struct'
+               % TODO: Check dimensions of each field
                for i = 1:numel(fn)
                   val = x.(fn{i});
                   switch class(val)
                      case 'categorical'
-                        valstr = cellstr(val);
-                        n = numel(val);
-                        jsa = javaArray('java.lang.String',n);
-                        for j = 1:n
-                           jsa(j) = java.lang.String(valstr{j});
-                        end
+                        jsa = MatR.jstr(cellstr(val));
                         val = REXPFactor(RFactor(jsa));
                      case 'char'
                      case {'double'}
